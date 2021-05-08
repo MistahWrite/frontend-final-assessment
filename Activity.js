8e926f2133a3a648f29327d9638c38281d748381
Chad Jay Harrington <MistahWrite@gmail.com>
2021-05-08 13:29:52 -0600

    Working on Activity Class

--- Activity.js
+++ Activity.js
@@ -1,43 +1,60 @@
 window.onload=function() {
     const start = document.getElementById("start");
     start.addEventListener("click", startActivity);
+    console.log("end window.onload function");
 }
 
 function startActivity(e) {
+    console.log("startActivitiy(" + e + ")");
     e.preventDefault();
     const category = "Meditate"; //document.getElementById("")
-    const accomplishment = document.getElementById("accomplishment");
-    const minutes = document.getElementById("minutes");
-    const seconds = document.getElementById("seconds");
+    const accomplishment = document.getElementById("accomplishment").value;
+    const minutes = document.getElementById("minutes").value;
+    const seconds = document.getElementById("seconds").value;
     const ActivityDiv = document.getElementById("Activity");
-    var Activity1 = new Activity(category, accomplishment, minutes, seconds);
-    Activity1.startTimer;
+
     ActivityDiv.innerHTML = "${accomplishment}<br/>";
     ActivityDiv.innerHTML = "<div id='timer'>${minutes}:${seconds}</div><br/>";
     ActivityDiv.innerHTML = "<input type='submit' id='startBtn' value='START'>";
+
+    console.log("Creating new Activitiy");
+    var Activity1 = new Activity(category, accomplishment, minutes, seconds);
+    const startBtn = document.getElementById("startBtn");
+    console.log("Adding Event Listener for Activity1.startTimer()");
+    startBtn.addEventListener("click", Activity1.startTimer);
+    console.log("Returning from adding Event Listener");
 }
 
 class Activity {
-    constructor: function(_category, _description, _minutes, _seconds) {
+    constructor(_category, _description, _minutes, _seconds) {
         this.completed = false;
         this.category = _category;
         this.description = _description;
         this.minutes = _minutes;
+        this.minutes = 5;
+        if (Number.isNaN(this.minutes) || this.minutes == undefined) {
+            this.minutes = 5;
+        }
         this.seconds = _seconds;
-        static this.id;
+        this.seconds = 0;
+        if (Number.isNaN(this.seconds) || this.seconds == undefined) {
+            this.seconds = 0;
+        }
+        //static this.id;
         this.id = this.id + 1;    
     }
 
-    startTimer: function() {
-        this.saveToStorage();
+    startTimer() {
+        //saveToStorage();
         var now = new Date().getTime();
-        var countDownDate = new.Date().getTime();
+        var countDownDate = new Date().getTime() + 1000 * this.seconds + this.minutes * 60 * 1000;
         var x = setInterval(function() {
             var distance = countDownDate - now;
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));4
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            var distance_minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var distance_seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-            document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+            console.log("Putting Timer On " + this.minutes + ":" + this.seconds);
+            document.getElementById("timer").innerHTML = distance_minutes + ":" + distance_seconds;
 
             if(distance < 0) {
                 clearInterval(x);
@@ -46,10 +63,10 @@ class Activity {
             }
         }, 1000);
     }
-    markComplete:  function() {
+    markComplete() {
         this.completed = true;
     }
-    saveToStorage: function() {
+    saveToStorage() {
         var objectToStore = {category: '${this.category}', description: '${this.description}', minutes: '${this.minutes}', seconds: '${this.seconds}'};
         var stringifiedObject = JSON.stringify(objectToStore);
         localStorage.setItem('Activity', stringifiedObject);
